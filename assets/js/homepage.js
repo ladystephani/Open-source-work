10d906125563779623a3221415d8fa8e0cf12251
Jessie Li <jessieli@jessieli.com>
2021-09-09 14:50:12 +0800

    Fetch response from GitHub API in hardcoded values

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,9 @@
-
 const getUserRepos = function () {
-    fetch("https://api.github.com/users/ladystephani/repos");
+    fetch("https://api.github.com/users/ladystephani/repos").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+    console.log("outside");
 };
 getUserRepos();
