0d5e537b73645db6405c11031b623cc56ab61ffe
Jessie Li <jessieli@jessieli.com>
2021-09-10 08:46:11 +0800

    Use event handler to call getUserRepos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,4 +9,21 @@ const getUserRepos = function(user) {
         })
     })
 };
-getUserRepos("ladystephani");// pass in localStorage value
+// getUserRepos();// moved to handler function on form submission
+
+const userFormEl = document.querySelector("#user-form");// #1
+const nameInputEl = document.querySelector("#username");// #2
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // store value from inputEl
+    const username = nameInputEl.value.trim();// #2 record value from the input box
+
+    if (username) {
+        getUserRepos(username);// pass in localStorage value
+        nameInputEl.value = "";// clear
+    } else {
+        alert("Please enter a username");
+    }
+}
+userFormEl.addEventListener("submit", formSubmitHandler);// #1 event listener on the form
